aefcd12b6e5ab5478f020ac88971d27185624f2b
94276 <942768654@qq.com>
2020-03-03 17:12:04 +0800

    初始化seata-test项目

---
+++ src/main/java/com/sun/test/App.java
@@ -0,0 +1,28 @@
+package com.sun.test;
+
+import com.zaxxer.hikari.HikariDataSource;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+
+import javax.sql.DataSource;
+
+/**
+ * Hello world!
+ *
+ */
+@SpringBootApplication
+public class App 
+{
+    public static void main( String[] args )
+    {
+        SpringApplication.run(App.class,args);
+    }
+
+    @Bean
+    @ConfigurationProperties(prefix = "spring.datasource.hikari")
+    public DataSource dataSource() {
+        return new HikariDataSource();
+    }
+}
